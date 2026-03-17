9b8bb92578b5a5f27c45c770eefe4507c3c62171
MehulBawadia <mehulbawadia@gmail.com>
2026-03-17 14:34:36 +0530

    Feat: Setup backend
    Set up the backend files and the folder structure.

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  }),
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
