4b0f7f176856d08269a8c95ba8689d365b5ede17
MehulBawadia <mehulbawadia@gmail.com>
2026-03-17 13:08:40 +0530

    Feat: Register UI
    Added the UI for the register page.
    Added the profile photo selector component.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Enter your full Name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Enter a password.");
+      return;
+    }
+
+    setError(null);
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text0-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-1.25 mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleRegister}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              type="text"
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John Doe"
+            />
+
+            <Input
+              type="text"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="johndoe@example.com"
+            />
+
+            <div className="col-span-2">
+              <Input
+                type="password"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Secret"
+              />
+            </div>
+          </div>
+
+          {error && <p className="text-red-500 pb-2.5">{error}</p>}
+
+          <button className="btn-primary uppercase">Register</button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Register;
