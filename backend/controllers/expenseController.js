3299e3819b2530879215d7a302a6ae5377cbb299
MehulBawadia <mehulbawadia@gmail.com>
2026-03-18 15:41:49 +0530

    Feat: API for fetch all expense
    Added the API for fetching all the expenses of the currently auth user.

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,5 +1,27 @@
 const Expense = require("../models/Expense");
 
+exports.index = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const expenseList = await Expense.find({ userId }).sort({ date: -1 });
+
+    res.status(200).json({
+      status: "success",
+      message: "Expenses successfully fetched.",
+      data: expenseList,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: "error",
+      message: "Could not fetch expense list.",
+      data: {
+        error: err.message,
+      },
+    });
+  }
+};
+
 exports.store = async (req, res) => {
   const userId = req.user.id;
 
--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { store } = require("../controllers/expenseController");
+const { index, store } = require("../controllers/expenseController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.get("/", protect, index);
 router.post("/create", protect, store);
 
 module.exports = router;
