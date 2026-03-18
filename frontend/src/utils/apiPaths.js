61f9359031fe2cf712aa458e58b37dd61260bbac
MehulBawadia <mehulbawadia@gmail.com>
2026-03-18 17:11:53 +0530

    Feat: API paths and Axios instance
    Added the API paths.
    Created the axios instance.

---
+++ frontend/src/utils/apiPaths.js
@@ -0,0 +1,27 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const API_PATHS = {
+  AUTH: {
+    LOGIN: "/api/v1/auth/login",
+    REGISTER: "/api/v1/auth/register",
+    USER_INFO: "/api/v1/auth/user-info",
+  },
+  DASHBOARD: {
+    GET_DATA: "/api/v1/dashboard",
+  },
+  INCOME: {
+    INDEX: "/api/v1/income",
+    STORE: "/api/v1/income/create",
+    DESTROY: (incomeId) => `/api/v1/income/${incomeId}`,
+    DOWNLOAD_IN_EXCEL: "/api/v1/income/download-in-excel",
+  },
+  EXPENSE: {
+    INDEX: "/api/v1/expense",
+    STORE: "/api/v1/expense/create",
+    DESTROY: (expenseId) => `/api/v1/expense/${expenseId}`,
+    DOWNLOAD_IN_EXCEL: "/api/v1/expense/download-in-excel",
+  },
+  IMAGE: {
+    UPPLOAD: "/api/v1/upload-image",
+  },
+};
---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.log("Server error. Try again");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.log("Request timed out. Try again");
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
