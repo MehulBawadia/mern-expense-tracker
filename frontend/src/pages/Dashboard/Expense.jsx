ece47150a4643df01f6ab81b471b9518496eebaa
MehulBawadia <mehulbawadia@gmail.com>
2026-03-24 17:27:07 +0530

    Feat: Expense overview
    Added the Expense page with overview line chart.

--- frontend/src/pages/Dashboard/Expense.jsx
+++ frontend/src/pages/Dashboard/Expense.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import toast from "react-hot-toast";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import Overview from "../../components/Expense/Overview";
 
 const Expense = () => {
-  return <div>Expense</div>;
+  useUserAuth();
+
+  const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [expenseData, setExpenseData] = useState([]);
+
+  const fetchExpenseDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(API_PATHS.EXPENSE.INDEX);
+      if (response?.data?.status === "success") {
+        setExpenseData(response?.data?.data);
+      }
+    } catch (error) {
+      console.log("Could not fetch expense details.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleAddExpense = async (expense) => {
+    const { category, date, amount, icon } = expense;
+
+    if (category.length <= 0) {
+      toast.error("Category is required.");
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) < 0) {
+      toast.error("Amount must be a number greater than 0.");
+      return;
+    }
+
+    if (!date) {
+      toast.error("Date is required.");
+      return;
+    }
+
+    try {
+      const response = await axiosInstance.post(`${API_PATHS.EXPENSE.STORE}`, {
+        category,
+        amount,
+        date,
+        icon,
+      });
+
+      setOpenAddExpenseModal(false);
+      toast.success(response.data.message);
+
+      await fetchExpenseDetails();
+    } catch (error) {
+      console.log(
+        "Error adding message",
+        error.response?.data?.message || error.message,
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenseDetails();
+
+    return () => {};
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Expense">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <Overview
+              transactions={expenseData}
+              onAddExpense={() => setOpenAddExpenseModal(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Expense;
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -52,3 +52,17 @@ export const prepareIncomeBarChartData = (data = []) => {
 
   return chartData;
 };
+
+export const prepareExpenseLineChartData = (data = []) => {
+  const sortedData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date),
+  );
+
+  const chartData = sortedData.map((item) => ({
+    month: DateTime.fromISO(item?.date).toFormat("dd MMM"),
+    amount: item?.amount,
+    category: item?.category,
+  }));
+
+  return chartData;
+};
